e9621e493e3e7381c468896120567bb94e0b8481
Anton Lampenscherf <uq04iqyx@ciphiidae.physik.uni-erlangen.de>
2026-02-16 11:52:36 +0100

    updated some parameters

--- generate_stable_planets.py
+++ generate_stable_planets.py
@@ -6,9 +6,10 @@ def main():
 
     for filename in planet_files:
         print("Starting to calculate stable form of planet: " + filename)
-        initial_state = sl.InitialStatePreparer().load_unstable_planet(filename + ".dat")
-        rho_min = np.min(initial_state[:,7])
-        parameters = sl.Parameters.from_p(dimension = 3, gamma = 1.4, initial_state = initial_state, integration_time = 500_000, t_eval = np.linspace(0,500_000,200), eta = 1.2, alpha_pi = 1, beta_pi = 1, rho_min = rho_min)
+        initial_state = sl.InitialState.load_unstable_planet(filename + ".dat")
+        #rho_min = np.min(initial_state[:,7])
+        h = 1.2 * (5.873e24 / 245)**(1/3)
+        parameters = sl.Parameters.from_p(dimension = 3, gamma = 1.4, initial_state = initial_state, integration_time = 500_000, t_eval = np.array([500_000]), eta = 1.2, alpha_pi = 1, beta_pi = 1, h = h)
         solver = sl.Solver(parameters=parameters,use_artificial_viscosity=True,use_gravity= True)
         solver.run(filename="./planets/" + filename + "_stable.dat")
 
--- simulate_planet_collision_glancing_rotation.py
+++ simulate_planet_collision_glancing_rotation.py
@@ -2,16 +2,17 @@ import solver as sl
 import numpy as np
 from pathlib import Path
 def main():
-    filename = "300_300_glancing_rotation"
-    planet = "Planet300_stable"
+    filename = "600_600_glancing_rotation"
+    planet = "Planet600_stable"
 
     print("Initializing Simulation")
     planet1 = sl.InitialState(sl.InitialState.load_stable_planet(planet + ".dat")).add_rotation([0,0,2 * np.pi / (24 * 3600)]).move_center_from_origin(R=[-2e9,0.9e8,0]).add_boost(v=[2e4,0,0])
     planet2 = sl.InitialState(sl.InitialState.load_stable_planet(planet + ".dat")).add_rotation([0,0,- 2 * np.pi / (24 * 3600)]).move_center_from_origin(R=[2e9,-0.9e8,0]).add_boost(v=[-2e4,0,0])
     initial_state = planet1.concatenate_initial_state(planet2).get_initial_state()
 
-    h = 1.2 * (5.873e24 / 245)**(1/3)
-    parameters = sl.Parameters.from_e(dimension = 3, gamma = 1.4, initial_state = initial_state[:,:8], integration_time = 500_000, t_eval = np.linspace(0,500_000,200), eta = 1.2, alpha_pi = 1, beta_pi = 1, h = h)#rho_min = rho_min)# rho_min = rho_min)
+    #h = 1.2 * (5.873e24 / 245)**(1/3)
+    rho_min = np.mean(initial_state[:,8])
+    parameters = sl.Parameters.from_e(dimension = 3, gamma = 1.4, initial_state = initial_state[:,:8], integration_time = 500_000, t_eval = np.linspace(0,500_000,200), eta = 1.2, alpha_pi = 1, beta_pi = 1, rho_min = rho_min)
     print("Start Simulation")
     solver = sl.Solver(parameters=parameters,use_artificial_viscosity=True,use_gravity= True)
     solver.run(filename = f"./simulation/{filename}.dat")
